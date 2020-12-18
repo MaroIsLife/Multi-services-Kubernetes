5b293275fed30100bfd3772dfaf28544b1080501
Marouane Ougnou <mougnou@e1r10p12.1337.ma>
2020-12-18 06:50:07 +0100

    Updating Grafana

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ docker build -f srcs/wordpress/Dockerfile -t image_wordpress .
 docker build -f srcs/nginx/Dockerfile -t image_nginx .
 docker build -f srcs/phpmyadmin/Dockerfile -t image_phpmyadmin .
 docker build -f srcs/mysql/Dockerfile -t image_mysql .
-kubectl create -f .
+docker build -f srcs/grafana/Dockerfile -t image_grafana .
+kubectl create -f ./srcs/yamls
