b8dbb01b56267fe6e4d7ef42433fd03be6ec5571
Marouane Ougnou <mougnou@e1r8p2.1337.ma>
2020-12-22 02:20:54 +0100

    Added External IP

--- srcs/grafana/script.sh
+++ srcs/grafana/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /grafana-7.3.6/bin
-grafana-server
+# cd /grafana-7.3.6/bin
+# grafana-server
+grafana-server -config /usr/share/grafana/conf/defaults.ini
--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ minikube start --extra-config=apiserver.service-node-port-range=1-30000
 eval $(minikube -p minikube docker-env)
 minikube addons enable metallb
 minikube addons enable metrics-server 
-EXPORT KUBE_IP = $(minikube ip)
+export KUBE_IP = $(minikube ip)
+#METALLB
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-# On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 docker build -f srcs/wordpress/Dockerfile -t image_wordpress .
