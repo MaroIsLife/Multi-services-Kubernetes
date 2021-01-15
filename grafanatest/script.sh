72d67711c4295b3d5a77fe83cf902e4f54083271
Marouane Ougnou <mougnou@e1r8p2.1337.ma>
2021-01-15 19:17:22 +0100

    Removed junk files

--- grafanatest/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-usr/sbin/./influxd&
-influx -execute "CREATE DATABASE influxdb"
-influx -execute "CREATE USER Maro WITH PASSWORD 'maro12345' WITH ALL PRIVILEGES"
-/usr/bin/./telegraf&
-cd usr/share/grafana/conf/
-grafana-server& -config /usr/share/grafana/conf/defaults.ini
