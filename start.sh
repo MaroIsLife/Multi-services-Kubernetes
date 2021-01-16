0880b69817137d079862334b6f5700a48268c74d
Marouane Ougnou <mougnou@e1r8p2.1337.ma>
2021-01-16 18:50:24 +0100

    Fixed Reverse and Redirection

--- start.sh
+++ start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 export MINIKUBE_HOME=/goinfre/mougnou
-# docker-machine env default
-# eval $(docker-machine env default)
-# minikube delete
 minikube start --driver virtualbox 
-# --extra-config=apiserver.service-node-port-range=1-30000
 eval $(minikube -p minikube docker-env)
 minikube addons enable metallb
 minikube addons enable metrics-server 
-export KUBE_IP=$(minikube ip)
+# export KUBE_IP=$(minikube ip)
 #METALLB
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
@@ -23,4 +19,4 @@ docker build -f srcs/influxdb/Dockerfile -t image_influxdb .
 kubectl apply -f srcs/yamls/configmap.yaml
 kubectl create -f srcs/yamls
 
-# DELETE ALL NORDPORTS FROM YAML FILES!!!!!
+# My IP = 192.168.99.242
