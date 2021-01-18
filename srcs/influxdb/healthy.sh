71e0b41a6e6de4531d3bcacca0c5b5a527b27059
Marouane Ougnou <mougnou@e1r11p2.1337.ma>
2021-01-18 12:22:38 +0100

    Fixed Liveness Probe and fixed phpmyadmin errors

---
+++ srcs/influxdb/healthy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 0 = healthy
+# 1 = unhealthy
+export p1=$(ps | grep influx | grep -v grep | wc -l) #should be 1
+export p2=$(ps | grep telegraf | grep -v grep | wc -l) # should be 1
+
+if [ $p1 -eq 1 ] && [ $p2 -eq 1 ];
+then
+    return 0
+else
+    return 1
+fi
--- srcs/nginx/healthy.sh
+++ srcs/nginx/healthy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # 0 = healthy
 # 1 = unhealthy
-export p1$(ps | grep php-fpm7 | grep -v grep | wc -l) #should be 3
-export p2$(ps | grep telegraf | grep -v grep | wc -l) # should be 1
-export p3$(ps | grep nginx | grep -v grep | wc -l) # Should be 6
+export p1=$(ps | grep php-fpm7 | grep -v grep | wc -l) #should be 3
+export p2=$(ps | grep telegraf | grep -v grep | wc -l) # should be 1
+export p3=$(ps | grep nginx | grep -v grep | wc -l) # Should be 6
+
 
 if [ $p1 -eq 3 ] && [ $p2 -eq 1 ] && [ $p3 -eq 6 ];
 then
