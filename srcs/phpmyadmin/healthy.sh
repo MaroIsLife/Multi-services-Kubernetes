40448db1d4cc168a5ce288a111a941597269ae93
Marouane Ougnou <mougnou@e1r8p2.1337.ma>
2021-01-19 11:39:10 +0100

    Updating Healthy.sh

--- srcs/phpmyadmin/healthy.sh
+++ srcs/phpmyadmin/healthy.sh
@@ -4,10 +4,10 @@
 export p1=$(ps | grep php-fpm7 | grep -v grep | wc -l) #should be 3
 export p2=$(ps | grep telegraf | grep -v grep | wc -l) # should be 1
 # export p3=$(ps | grep nginx | grep -v grep | wc -l) # Should be 6
-export p4=$(ps | grep sshd | grep -v grep | wc -l) # Should be 1
+# export p4=$(ps | grep sshd | grep -v grep | wc -l) # Should be 1
 
 
-if [ $p1 -eq 3 ] && [ $p2 -eq 1 ] && [ $p4 -eq 1 ];
+if [ $p1 -eq 3 ] && [ $p2 -eq 1 ];
 then
     return 0
 else
